2004b8083553eb6af1adc68e1bd2d6781e87880b
yogesh <39838149+yogesh-m-2@users.noreply.github.com>
2025-02-03 17:27:08 +0530

    update of both frontend and backend

--- frontend/src/components/navigation/Navigation.js
+++ frontend/src/components/navigation/Navigation.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
-import { FaBars, FaTimes, FaHome, FaUser, FaCog } from 'react-icons/fa'; // Icons
+import { FaBars, FaTimes, FaHome, FaUser, FaCog, FaSignOutAlt  } from 'react-icons/fa'; // Icons
 import '../../styles/navigation/Navigation.css';
+import { useAuth } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Navigation = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const { isAuthenticated, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout(); // Call logout from context
+    navigate('/admin'); // Redirect to login page
+  };
 
   return (
     <nav className={`navigation ${isCollapsed ? 'collapsed' : ''}`}>
@@ -17,6 +26,13 @@ const Navigation = () => {
         <li><FaHome className="icon" /> {!isCollapsed && 'Home'}</li>
         <li><FaUser className="icon" /> {!isCollapsed && 'Profile'}</li>
         <li><FaCog className="icon" /> {!isCollapsed && 'Settings'}</li>
+        {isAuthenticated && (
+          <li>
+            <button onClick={handleLogout} className="logout-btn">
+              <FaSignOutAlt className="icon" /> {!isCollapsed && 'Logout'}
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
