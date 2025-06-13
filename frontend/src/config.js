171e7ad74ce6744a5cb435ed51165ec33b4353b4
Pugazheshwar <pugazheshwar411@gmail.com>
2025-06-13 12:58:05 +0530

    BugFix: Socket Not Working Due to Local IP

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 
 const config = {
     BASE_URL: "http://127.0.0.1:8142",
-    Socket_URL: "127.0.0.1:8142", // Backend API base URL
+    Socket_URL: "170.187.200.195:8142", // Backend API base URL
      // Backend API base URL
   };
   
