c07e03222df3901074dd0086999eae705126ec94
D.PUGAZHESHWAR <pugazheshwar411@gmail.com>
2025-06-24 15:11:03 +0530

    Revert "New Api is Added for OUT PATIENT"

--- backend/Neuro-Canteen-1/src/main/java/com/neuroCanteen/serviceImpl/OrderServiceImpl.java
+++ backend/Neuro-Canteen-1/src/main/java/com/neuroCanteen/serviceImpl/OrderServiceImpl.java
@@ -171,9 +171,4 @@ public class OrderServiceImpl implements OrderService {
         }).orElseThrow(() -> new RuntimeException("Order not found"));
         
     }
-
-    @Override
-    public List<Order> getOrdersByPhoneNoAndRole(String phoneNo, String orderedRole) {
-        return orderRepository.findByPhoneNoAndOrderedRole(phoneNo, orderedRole);
-    }
 }
